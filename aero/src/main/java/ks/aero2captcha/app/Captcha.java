433be0c78e2117c6ac6ddb04e826932dd428cbda
kamilszewczyk <szewczyk.kamil@gmail.com>
2014-04-18 21:49:53 +0200

    Allow to zoom in/out on captcha image

--- aero/src/main/java/ks/aero2captcha/app/Captcha.java
+++ aero/src/main/java/ks/aero2captcha/app/Captcha.java
@@ -1,22 +1,17 @@
 package ks.aero2captcha.app;
 
-import android.app.NotificationManager;
-import android.app.PendingIntent;
-import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.os.Handler;
 import android.preference.PreferenceManager;
-import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -25,6 +20,7 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import ks.aero2captcha.alarm.CaptchaService;
+import ks.aero2captcha.image.TouchImageView;
 import ks.aero2captcha.network.BaseAsyncTask;
 import ks.aero2captcha.network.ConnectionManager;
 import ks.aero2captcha.network.State;
@@ -140,7 +136,8 @@ public class Captcha extends ActionBarActivity {
                 submit.setEnabled(false);
             }
             else if(rs.code == TaskResult.CODE_SUCCESS) {
-                ImageView image = (ImageView) findViewById(R.id.captchaImage);
+                TouchImageView image = (TouchImageView) findViewById(R.id.captchaImage);
+                image.setMinZoom(0.1f);
                 image.setImageBitmap((Bitmap) rs.getResultData());
 
                 submit.setEnabled(true);
